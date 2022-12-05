ba46d7ca3f1d7f3570874b75848985edf59718fb
mathieuZ <mathieu.ziajka@symaris.fr>
2022-12-05 16:39:15 +0100

    feat: auth & user file and folder creation adding user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './service/user.service';
-import { UserController } from './user.controller';
+import { UserController } from './controller/user.controller';
 
 @Module({
   providers: [UserService],
