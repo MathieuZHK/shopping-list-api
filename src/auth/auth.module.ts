7ab240c154a16581979855b71fbe87710c9edde7
mathieuZ <mathieu.ziajka@symaris.fr>
2022-12-08 11:15:10 +0100

    fix vercel

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { UserRepository } from 'src/user/repository/userRepository';
-import { UserService } from 'src/user/service/user.service';
+import { UserRepository } from '../user/repository/userRepository';
+import { UserService } from '../user/service/user.service';
 import { AuthController } from './controller/auth.controller';
 import { AuthService } from './service/auth.service';
 import { AtStrategy, RtStrategy } from './strategies';
